769e4b98ec71d7d1af01823ca5bcd961632f4605
Andree Valle Campos <avallecam@gmail.com>
2025-04-12 17:29:27 +0100

    add script and data for mers

---
+++ instructors/data-severity-stratified.R
@@ -0,0 +1,37 @@
+# mers
+# stratified by groups
+
+# Load packages -----------------------------------------------------------
+library(cfr)
+library(epiparameter)
+library(tidyverse)
+
+
+# Read reported cases -----------------------------------------------------
+mers_sev <- outbreaks::mers_korea_2015$linelist %>%
+  as_tibble() %>%
+  dplyr::mutate(
+    age_category = base::cut(
+      x = age,
+      breaks = c(0, 70, 90), # replace with max value if known
+      include.lowest = TRUE,
+      right = FALSE
+    )
+  ) %>%
+  # skimr::skim(age)
+  # dplyr::count(age_class, age_category)
+  # converto to incidence2 object
+  incidence2::incidence(
+    date_index = c("dt_onset", "dt_death"),
+    groups = "age_category",
+    complete_dates = TRUE
+  ) %>%
+  # convert to cfr format
+  cfr::prepare_data(
+    cases_variable = "dt_onset",
+    deaths_variable = "dt_death"
+  )
+
+# save data
+mers_sev %>%
+  readr::write_rds(file.path("episodes", "data", "mers_byage.rds"))
