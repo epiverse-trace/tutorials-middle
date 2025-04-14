38d13162b304e8b010165f5c00e30549f065e50d
Andree Valle Campos <avallecam@gmail.com>
2025-04-14 18:59:40 +0100

    simplify generation code

--- instructors/data-severity-stratified.R
+++ instructors/data-severity-stratified.R
@@ -8,7 +8,7 @@ library(tidyverse)
 
 
 # Read reported cases -----------------------------------------------------
-mers_sev <- outbreaks::mers_korea_2015$linelist %>%
+mers_linelist <- outbreaks::mers_korea_2015$linelist %>%
   tibble::as_tibble() %>%
   dplyr::mutate(
     age_category = base::cut(
@@ -18,6 +18,9 @@ mers_sev <- outbreaks::mers_korea_2015$linelist %>%
       right = FALSE
     )
   ) %>%
+  dplyr::select(id, age_category, dt_onset, dt_death)
+
+mers_sev <- mers_linelist %>%
   # skimr::skim(age)
   # dplyr::count(age_class, age_category)
   # converto to incidence2 object
@@ -26,6 +29,7 @@ mers_sev <- outbreaks::mers_korea_2015$linelist %>%
     groups = "age_category",
     complete_dates = TRUE
   ) %>%
+  # plot()
   # convert to cfr format
   cfr::prepare_data(
     cases_variable = "dt_onset",
@@ -45,23 +49,11 @@ readr::read_rds(file.path(
 
 # alternative ------------------------------------------------------------
 
-mers_linelist <- outbreaks::mers_korea_2015$linelist %>%
-  tibble::as_tibble() %>%
-  dplyr::mutate(
-    age_category = base::cut(
-      x = age,
-      breaks = c(10, 30, 50, 70, 90), # replace with max value if known
-      include.lowest = TRUE,
-      right = FALSE
-    )
-  ) %>%
-  dplyr::select(id, age_category, dt_onset, dt_death)
-
 mers_linelist %>% 
   # converto to incidence2 object
   incidence2::incidence(
     date_index = c("dt_onset", "dt_death"),
-    groups = "age_category",
+    # groups = "age_category",
     complete_dates = TRUE
   ) %>%
   # plot()
